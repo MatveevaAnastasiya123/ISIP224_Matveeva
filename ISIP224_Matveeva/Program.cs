daf6c29beffcfece02b02227402ee6458b414467
Anastasia <nastammm4"gmail.com>
2026-09-09 22:52:45 +0300

    Сделано меню и выход

--- ISIP224_Matveeva/Program.cs
+++ ISIP224_Matveeva/Program.cs
@@ -10,8 +10,32 @@ namespace ISIP224_Matveeva
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите кол-во операций от 2 до 40");
+            int count = Convert.ToInt16(Console.ReadLine());
+            if(count<2 || count > 40)
+            {
+                Console.WriteLine("Ошибка. Введите кол-во операций от 2 до 40");
+                return;
+            }
+            string[] names = new string[count];
+            double[] prices = new double[count];
 
+            Console.WriteLine("Введите данные по очереди:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Трата {i + 1} (Название; Цена): ");
+                string[] parts = Console.ReadLine().Split(';');
+                names[i] = parts[0].Trim();
+                prices[i] = Convert.ToDouble(parts[1].Trim());
+            }
+            while (true)
+            {
+                Console.WriteLine("\n1-Вывод, 2-Статистика, 3-Сортировка, 4-Конвертация, 5-Поиск, 0-Выход");
+                Console.Write("Выбор: ");
+                string choice = Console.ReadLine();
 
+                if (choice == "0") break;
+            }
+            }
         }
-    }
 }
